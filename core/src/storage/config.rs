f39fceb9446e12e9d0558c152b7e26cd4e8033be
SByte <stormybytes@gmail.com>
2024-10-27 04:28:02 +0700

    WIP Config trait

--- core/src/storage/config.rs
+++ core/src/storage/config.rs
@@ -1,40 +1,52 @@
-use std::io::{Read, Write};
+use std::collections::HashMap;
+use std::fmt::Debug;
 
-use anyhow::{bail, Result};
-use serde::Serialize;
-use yaml_rust2::{YamlEmitter, YamlLoader};
+pub trait Config: Debug {
+    //
+    // GETTERS
+    //
 
-use super::utils;
+    fn get_bool(&self, setting: &str) -> Option<bool>;
 
-pub fn save_config<T>(name: &str, config: T) -> Result<()>
-where
-    T: Serialize,
-{
-    // Read from existing configs
-    let save = utils::get_save_default(".tueconf");
-    if save.is_none() {
-        bail!("Config file not found!");
+    fn get_i64(&self, setting: &str) -> Option<i64>;
+
+    fn get_f64(&self, setting: &str) -> Option<f64>;
+
+    fn get_str(&self, setting: &str) -> Option<&str>;
+
+    fn get_vec_str(&self, setting: &str) -> Option<&Vec<&str>>;
+
+    fn get_vec_str_mut(&mut self, setting: &str) -> Option<&mut Vec<&str>>;
+
+    //
+    // SETTERS
+    //
+
+    /// Returns true if the setting existed and modifying it was successful
+    fn set_bool(&mut self, setting: &str, value: bool) -> bool;
+
+    /// Returns true if the setting existed and modifying it was successful
+    fn set_i64(&mut self, setting: &str, value: i64) -> bool;
+
+    /// Returns true if the setting existed and modifying it was successful
+    fn set_f64(&mut self, setting: &str, value: f64) -> bool;
+
+    /// Returns true if the setting existed and modifying it was successful
+    fn set_str(&mut self, setting: &str, value: &str) -> bool;
+}
+
+pub struct ConfigStorage {
+    data: HashMap<String, Box<dyn Config>>,
+}
+
+impl ConfigStorage {
+    pub fn get(&self, config_group: &str) -> Option<&dyn Config> {
+        self.data.get(config_group).map(|c| c.as_ref())
     }
-    let mut save = save.unwrap();
-    let mut content = String::new();
-    save.read_to_string(&mut content)?;
 
-    let mut yaml = YamlLoader::load_from_str(&content)?;
-    let yaml = &mut yaml[0];
-
-    // Merge new config into configs
-    let selected_yaml = &mut yaml[name];
-    let new_yaml = YamlLoader::load_from_str(&serde_yaml_ng::to_string(&config)?)?
-        .first()
-        .unwrap()
-        .clone(); // Lord forgive me for what I have done here
-    *selected_yaml = new_yaml;
-
-    // Write it back into configs
-    save.set_len(0)?;
-    let mut s = String::new();
-    YamlEmitter::new(&mut s).dump(yaml).unwrap();
-    save.write_all(s.as_bytes())?;
-
-    Ok(())
+    pub fn get_mut<'a>(&'a mut self, config_group: &str) -> Option<&'a mut (dyn Config + 'a)> {
+        self.data
+            .get_mut(config_group)
+            .map::<&'a mut dyn Config, _>(|c| c.as_mut())
+    }
 }
