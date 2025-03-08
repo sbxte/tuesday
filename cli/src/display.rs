be26026b51d89f68e4ce18801cbacb98dc2ef543
DaringCuteSeal <daringcuteseal@gmail.com>
2025-03-08 18:36:54 +0700

    fix(cli): oopws fix floating point arithmetic issue in calendar task completion

--- cli/src/display.rs
+++ cli/src/display.rs
@@ -151,7 +151,7 @@ pub fn print_calendar(graph: &Graph, date: &NaiveDate) -> AppResult<()> {
                 let range_finished = if node.metadata.children.len() == 0 {
                     0
                 } else {
-                    finished / node.metadata.children.len() * 5
+                    finished * 5 / node.metadata.children.len()
                 };
 
                 let color = HEATMAP_PALLETE[range_finished];
